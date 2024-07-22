8720c7e8ead0c400ac741d6c7b79d442fccd25ed
kkukileon305 <kkukileon305@gmail.com>
2024-07-22 15:50:56 +0900

    redirect main page when code is null

--- app/(auth)/callback/google/page.tsx
+++ app/(auth)/callback/google/page.tsx
@@ -10,34 +10,33 @@ const Page = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
 
-  if (code === null) {
-    router.push("/");
-    router.refresh();
-    return <></>;
-  }
-
   const onSignInWithCode = async () => {
-    try {
-      const {
-        data: { accessToken, refreshToken },
-      } = await axiosInstance.get<TokenType>(
-        `/v0.2/auth/google/callback?code=${code}`
-      );
+    if (code !== null) {
+      try {
+        const {
+          data: { accessToken, refreshToken },
+        } = await axiosInstance.get<TokenType>(
+          `/v0.2/auth/google/callback?code=${code}`
+        );
 
-      const today = new Date();
+        const today = new Date();
 
-      setCookie("accessToken", accessToken, {
-        expires: new Date(today.getTime() + 3600000 * 24 * 1),
-      });
+        setCookie("accessToken", accessToken, {
+          expires: new Date(today.getTime() + 3600000 * 24 * 1),
+        });
 
-      setCookie("refreshToken", refreshToken, {
-        expires: new Date(today.getTime() + 3600000 * 24 * 7),
-      });
+        setCookie("refreshToken", refreshToken, {
+          expires: new Date(today.getTime() + 3600000 * 24 * 7),
+        });
 
+        router.push("/");
+        router.refresh();
+      } catch (e) {
+        // router.push("/");
+      }
+    } else {
       router.push("/");
       router.refresh();
-    } catch (e) {
-      // router.push("/");
     }
   };
 
