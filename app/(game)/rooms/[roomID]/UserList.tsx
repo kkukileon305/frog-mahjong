26e5300b7d8b85ea6a5e97cf022c7003325834a4
kkukileon305 <kkukileon305@gmail.com>
2024-08-02 17:19:30 +0900

    add framer motion and change roomId to roomID

--- app/(game)/rooms/[roomID]/UserList.tsx
+++ app/(game)/rooms/[roomID]/UserList.tsx
@@ -5,26 +5,24 @@ import { UserSocket } from "@/utils/socketTypes";
 
 const UserList = ({ user }: { user: UserSocket }) => {
   return (
-    <>
-      <motion.div
-        className="p-2 bg-blue-400 text-white font-bold rounded-xl mb-2"
-        initial={{
-          opacity: 0,
-          x: -300,
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        exit={{
-          opacity: 0,
-          x: -300,
-        }}
-      >
-        <p>{user.name}</p>
-        <p>{user.email}</p>
-      </motion.div>
-    </>
+    <motion.div
+      className="p-2 bg-blue-400 text-white font-bold rounded-xl mb-2"
+      initial={{
+        opacity: 0,
+        x: -300,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+      }}
+      exit={{
+        opacity: 0,
+        x: -300,
+      }}
+    >
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+    </motion.div>
   );
 };
 
