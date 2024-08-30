3051e66a4d0f2bf381b8076b51989fa301170786
kkukileon305 <kkukileon305@gmail.com>
2024-08-30 13:24:46 +0900

    change rooms loading ui

--- app/(game)/rooms/RoomLoading.tsx
+++ app/(game)/rooms/RoomLoading.tsx
@@ -8,13 +8,9 @@ const RoomLoading = () => {
           height: `${82 * SIZE - 16}px`,
         }}
         className="bg-gray-200 mb-4 animate-pulse flex justify-center items-center rounded-xl"
-      >
-        読み込み中...
-      </div>
+      />
 
-      <div className="h-8 flex flex-col items-center justify-center">
-        読み込み中...
-      </div>
+      <div className="h-8 flex flex-col items-center justify-center bg-gray-200 rounded-xl animate-pulse" />
     </>
   );
 };
