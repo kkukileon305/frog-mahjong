d9e7c4325d7ea82938728ee219890de6207818f9
kkukileon305 <kkukileon305@gmail.com>
2024-09-07 03:11:40 +0900

    add next-i18n module

--- i18n/request.tsx
+++ i18n/request.tsx
@@ -2,12 +2,10 @@ import { getRequestConfig } from "next-intl/server";
 import { getCookie } from "cookies-next";
 import { KR } from "@/utils/const";
 
-const COOKIE_NAME = "NEXT_LOCALE";
+const LOCALE_COOKIE_NAME = "NEXT_LOCALE";
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = getCookie(COOKIE_NAME)?.toString() || KR;
+  const locale = getCookie(LOCALE_COOKIE_NAME)?.toString() || KR;
 
   return {
     locale,
