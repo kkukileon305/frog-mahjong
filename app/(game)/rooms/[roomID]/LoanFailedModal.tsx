e2fea183e6a77173c52c406fad88f9b6bf983bd3
kkukileon305 <kkukileon305@gmail.com>
2024-09-07 04:54:48 +0900

    split LoanFailedModal.tsx.tsx component messages in kr.json

--- app/(game)/rooms/[roomID]/LoanFailedModal.tsx
+++ app/(game)/rooms/[roomID]/LoanFailedModal.tsx
@@ -4,6 +4,7 @@ import React, { Dispatch, SetStateAction } from "react";
 import { UserSocket } from "@/utils/socketTypes";
 import cards from "@/app/(game)/rooms/[roomID]/game/cards";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 type LoanFailedModalProps = {
   isLoanFailed: number;
@@ -16,6 +17,8 @@ const LoanFailedModal = ({
   isLoanFailed,
   users,
 }: LoanFailedModalProps) => {
+  const m = useTranslations("LoanFailedModal");
+
   const failedUser = users?.find((user) => user.id === isLoanFailed)!!;
   const cardImages = failedUser.cards?.map(
     (card) => cards.find((ci) => ci.id === card.cardID)!!
@@ -29,7 +32,8 @@ const LoanFailedModal = ({
     <div className="absolute flex justify-center items-center w-full h-full left-0 top-0 bg-black/50 z-10">
       <div className="bg-white max-w-3xl w-full rounded-xl p-8">
         <h3 className="text-3xl font-bold mb-8 text-center">
-          {failedUser.name}님이 론을 실패하여 패가 한번 공개됩니다
+          {failedUser.name}
+          {m("title")}
         </h3>
 
         <div className="flex gap-2 justify-center">
@@ -48,7 +52,7 @@ const LoanFailedModal = ({
           onClick={init}
           className="w-full bg-sky-500 rounded-lg py-3 text-white font-bold disabled:bg-gray-400 mt-4"
         >
-          닫기
+          {m("back")}
         </button>
       </div>
     </div>
