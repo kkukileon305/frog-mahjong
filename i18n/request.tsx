92522e5d338464d88d95ec2d4b8b79adaeca6fc5
kkukileon305 <kkukileon305@gmail.com>
2024-09-07 05:53:18 +0900

    change locale cookie name

--- i18n/request.tsx
+++ i18n/request.tsx
@@ -2,7 +2,7 @@ import { getRequestConfig } from "next-intl/server";
 import { getCookie } from "cookies-next";
 import { KR } from "@/utils/const";
 
-const LOCALE_COOKIE_NAME = "NEXT_LOCALE";
+const LOCALE_COOKIE_NAME = "LOCALE_NAME";
 
 export default getRequestConfig(async () => {
   const locale = getCookie(LOCALE_COOKIE_NAME)?.toString() || KR;
