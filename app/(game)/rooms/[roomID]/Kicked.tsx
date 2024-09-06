d3642b2f285794c38f409b327990be31c6a9cefc
kkukileon305 <kkukileon305@gmail.com>
2024-09-07 04:50:54 +0900

    split Kicked.tsx component messages in kr.json

--- app/(game)/rooms/[roomID]/Kicked.tsx
+++ app/(game)/rooms/[roomID]/Kicked.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import React from "react";
+import { useTranslations } from "next-intl";
 
 const KickedGame = () => {
+  const m = useTranslations("KickedGame");
+
   return (
     <div className="flex h-[calc(100dvh)] bg-gray-200 justify-center items-center">
       <div className="p-4 w-full max-w-3xl bg-white rounded-xl flex justify-center items-center flex-col">
-        <p className="font-bold text-3xl">강제퇴장 되었습니다</p>
+        <p className="font-bold text-3xl">{m("title")}</p>
         <Link
           className="py-2 px-4 mt-4 border border-blue-400 rounded-xl text-blue-400 font-bold"
           href={"/rooms"}
         >
-          돌아가기
+          {m("back")}
         </Link>
       </div>
     </div>
