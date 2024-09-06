6f636d982a53f0dc4263dc32101a028f85ea812a
kkukileon305 <kkukileon305@gmail.com>
2024-09-07 03:52:48 +0900

    split sign up done page messages in kr.json

--- app/(auth)/signup/done/page.tsx
+++ app/(auth)/signup/done/page.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
+import { getTranslations } from "next-intl/server";
 
-const Page = () => {
+const Page = async () => {
+  const m = await getTranslations("SignUp");
+
   return (
     <div className="bg-slate-50">
       <div className="max-w-xl mx-auto py-24">
-        <h2 className="text-4xl text-center mb-6">회원가입성공.</h2>
+        <h2 className="text-4xl text-center mb-6">{m("successSignUp")}</h2>
 
         <div className="flex justify-center">
           <Link
             href="/signin"
             className="border p-3 rounded border-blue-400 text-blue-400"
           >
-            로그인
+            {m("signIn")}
           </Link>
         </div>
       </div>
