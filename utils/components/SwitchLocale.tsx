1e0f2d27920c1db961665cccfc9802b68cd7bd3b
kkukileon305 <kkukileon305@gmail.com>
2024-09-07 14:06:26 +0900

    Create Switch Locale btn

--- utils/components/SwitchLocale.tsx
+++ utils/components/SwitchLocale.tsx
@@ -2,19 +2,47 @@
 
 import { useLocale } from "next-intl";
 import { setCookie } from "cookies-next";
-import { LOCALE_COOKIE_NAME } from "@/utils/constants/const";
+import { JP, KR, LOCALE_COOKIE_NAME, US } from "@/utils/constants/const";
 import { useRouter } from "next/navigation";
+import { IoLanguage } from "react-icons/io5";
+import { FaCheck } from "react-icons/fa";
+
+const locales = [KR, US];
 
 const SwitchLocale = () => {
   const locale = useLocale();
   const router = useRouter();
 
-  const onClick = () => {
-    setCookie(LOCALE_COOKIE_NAME, "us");
+  const onClick = (localeName: string) => {
+    setCookie(LOCALE_COOKIE_NAME, localeName);
     router.refresh();
   };
 
-  return <div onClick={onClick}>Change {locale} to eng</div>;
+  return (
+    <div className="relative group" tabIndex={0}>
+      <div className="cursor-pointer hover:bg-gray-200 group-focus:bg-gray-200 p-2 rounded-xl">
+        <IoLanguage size={24} />
+      </div>
+      <div className="absolute top-[calc(100%+4px)] right-0 invisible group-focus:visible flex flex-col border rounded-xl overflow-hidden drop-shadow-lg">
+        {locales.map((localeName) => (
+          <div
+            onClick={() => onClick(localeName)}
+            className="w-32 p-2 border-b last:border-none bg-white hover:bg-gray-200 cursor-pointer"
+            key={localeName}
+          >
+            <p
+              className={`flex gap-2 items-center ${
+                locale === localeName && "text-blue-400"
+              }`}
+            >
+              {localeName.toUpperCase()}
+              {locale === localeName && <FaCheck />}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SwitchLocale;
