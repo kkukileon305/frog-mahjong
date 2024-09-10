85ea21bccb7857ee6c5c4cc89f6d96930bdef255
kkukileon305 <kkukileon305@gmail.com>
2024-09-10 11:55:22 +0900

    add ui in error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import React from "react";
+
 const Error = () => {
-  return <div className="mx-auto max-w-3xl flex justify-center">에러</div>;
+  return (
+    <div className="flex h-[calc(100dvh)] justify-center items-center">
+      <p className="font-bold text-3xl">500</p>
+      <p>ERROR</p>
+    </div>
+  );
 };
 
 export default Error;
