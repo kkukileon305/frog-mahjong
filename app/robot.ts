9fa4a4c1a100d5a9240a88379ef17dab1f015ebc
kkukileon305 <kkukileon305@gmail.com>
2024-09-23 02:33:37 +0900

    change robot.ts

--- app/robot.ts
+++ app/robot.ts
@@ -4,17 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/signup",
-        "/signup/done",
-        "/signin",
-        "/callback/google",
-        "/forgot-password",
-        "/reset-password",
-        "/reset-password/done",
-        "/rooms",
-      ],
+      allow: "/$",
+      disallow: "/",
     },
   };
 }
