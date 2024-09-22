796b3272fdc14fc12f243c7ef5a94550e51a1c3d
kkukileon305 <kkukileon305@gmail.com>
2024-09-23 01:52:40 +0900

    allow all userAgent

--- app/robot.ts
+++ app/robot.ts
@@ -2,22 +2,20 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "Googlebot",
-        allow: "/",
-        disallow: [
-          "/signup",
-          "/signup/done",
-          "/signin",
-          "/callback/google",
-          "/forgot-password",
-          "/reset-password",
-          "/reset-password/done",
-          "/rooms",
-        ],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/signup",
+        "/signup/done",
+        "/signin",
+        "/callback/google",
+        "/forgot-password",
+        "/reset-password",
+        "/reset-password/done",
+        "/rooms",
+      ],
+    },
   };
 }
 // a 태그가 있는것만 봇이 추적해서 스캔?
