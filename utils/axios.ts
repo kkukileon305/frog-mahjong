0d928f75a5c966ff67c74fefb1736697a90607eb
kkukileon305 <kkukileon305@gmail.com>
2024-09-29 11:40:26 +0900

    test branch trigger

--- utils/axios.ts
+++ utils/axios.ts
@@ -86,6 +86,8 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
+// test dev
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (e) => {
