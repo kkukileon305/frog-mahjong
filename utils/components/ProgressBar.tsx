5fb85d9867659d736d88f18af4007c245f52ccf9
kkukileon305 <kkukileon305@gmail.com>
2024-10-01 18:24:27 +0900

    add ProgressBar.tsx

--- utils/components/ProgressBar.tsx
+++ utils/components/ProgressBar.tsx
@@ -4,7 +4,7 @@ type ProgressBarProps = {
 
 const ProgressBar = ({ progress }: ProgressBarProps) => {
   return (
-    <div className="relative w-full max-w-2xl mx-auto border-2 border-white bg-gray-400 rounded-full h-5 overflow-hidden">
+    <div className="relative w-full max-w-2xl mx-auto border-2 border-white bg-gray-400 rounded-full h-6 overflow-hidden">
       <div
         className="bg-white h-full rounded-full transition-all duration-500"
         style={{ width: `${progress}%` }}
