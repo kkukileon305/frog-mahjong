fec7ea032e6c123bf98f7cc913cef8a1fdd73e29
kkukileon305 <kkukileon305@gmail.com>
2024-10-22 18:11:22 +0900

    add input:checkbox that change gameType in SettingMenus.tsx

--- utils/components/SettingMenus.tsx
+++ utils/components/SettingMenus.tsx
@@ -10,6 +10,7 @@ import QuitModal from "@/app/(game)/rooms/QuitModal";
 import SignOutBtn from "@/utils/components/SignOutBtn";
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
+import useMatchSettingStore from "@/utils/stores/useMatchSettingStore";
 
 const SettingMenus = () => {
   const m = useTranslations("Settings");
@@ -23,6 +24,11 @@ const SettingMenus = () => {
     volume: s.volume,
     setVolume: s.setVolume,
     audios: s.audios,
+  }));
+
+  const { gameType, setGameType } = useMatchSettingStore((s) => ({
+    gameType: s.gameType,
+    setGameType: s.setGameType,
   }));
 
   const [debounceTimeout, setDebounceTimeout] = useState<NodeJS.Timeout | null>(
@@ -55,6 +61,10 @@ const SettingMenus = () => {
 
     return () => window.removeEventListener("mousedown", handler);
   }, []);
+
+  const handleGameTypeChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setGameType(e.currentTarget.checked ? "FROG_MAHJONG_OLD" : "FROG_MAHJONG");
+  };
 
   return (
     <>
@@ -112,6 +122,23 @@ const SettingMenus = () => {
               <Link className="p-2 block hover:bg-gray-200" href="/license">
                 {m("license")}
               </Link>
+            </div>
+
+            <div className="bg-gray-200 h-[1px]" />
+
+            <div className="bg-white w-full hover:bg-gray-200">
+              <label className="w-full inline-flex items-center cursor-pointer justify-center py-2">
+                <input
+                  type="checkbox"
+                  checked={gameType === "FROG_MAHJONG_OLD"}
+                  onChange={handleGameTypeChange}
+                  className="sr-only peer"
+                />
+                <div className="relative w-11 h-6 bg-gray-200 outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  use old
+                </span>
+              </label>
             </div>
 
             {refreshToken && (
