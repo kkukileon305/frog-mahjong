471cd29603bc3d726fd52e4ff284e3d615e2049b
kkukileon305 <kkukileon305@gmail.com>
2024-11-29 01:41:42 +0900

    add retry getToken when failed get token

--- utils/functions/registerServiceWorker.ts
+++ utils/functions/registerServiceWorker.ts
@@ -2,10 +2,10 @@ export function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const a = await navigator.serviceWorker.register(
-          "/firebase-messaging-sw.js"
-        );
-      } catch (e) {}
+        await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+      } catch (e) {
+        console.log(e);
+      }
     });
   }
 }
