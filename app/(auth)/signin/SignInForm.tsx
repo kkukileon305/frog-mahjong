0624fd207b213771e98677c3b7550b308f757a4c
kkukileon305 <kkukileon305@gmail.com>
2024-12-04 23:56:16 +0900

    add modal: duplicate login error

--- app/(auth)/signin/SignInForm.tsx
+++ app/(auth)/signin/SignInForm.tsx
@@ -27,6 +27,7 @@ const SignInForm = () => {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm<SignInInputs>();
 
   const {
@@ -37,14 +38,18 @@ const SignInForm = () => {
   } = usePreloadAssets();
 
   const [isSignInFailed, setIsSignInFailed] = useState(false);
+  const [isDuplicated, setIsDuplicated] = useState(false);
+
+  // 강제 로그인 중
+  const [isPending, setIsPending] = useState(false);
+
   const addNotification = useNotificationStore((s) => s.addNotification);
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit: SubmitHandler<SignInInputs> = async (inputs) => {
-    setIsLoading(true);
-
+  const signIn = async (inputs: SignInInputs) => {
     try {
+      setIsPending(true);
       const {
         data: { accessToken, refreshToken, userID, isDuplicateLogin },
       } = await axiosInstance.post<TokenType>("/v0.1/auth/signin", {
@@ -99,8 +104,44 @@ const SignInForm = () => {
         setIsSignInFailed(true);
         console.log('USER_ALREADY_EXISTED"');
       }
+    } finally {
+      setIsPending(false);
+    }
+  };
 
-      // TODO: 중복로그인 에러 확인
+  const onSubmit: SubmitHandler<SignInInputs> = async (inputs) => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await axiosInstance.post<boolean>(
+        "/v0.1/auth/signin/check",
+        {
+          email: inputs.email,
+          password: inputs.password,
+        }
+      );
+
+      if (data) {
+        setIsDuplicated(true);
+      } else {
+        await signIn(inputs);
+      }
+    } catch (e) {
+      const error = e as AxiosError<ErrorType>;
+
+      // login failed
+      if (
+        error.response?.data.errType === USER_NOT_FOUND ||
+        error.response?.data.errType === PASSWORD_NOT_MATCH
+      ) {
+        setIsSignInFailed(true);
+      }
+
+      if (error.response?.data.errType === "USER_ALREADY_EXISTED") {
+        setIsSignInFailed(true);
+        console.log('USER_ALREADY_EXISTED"');
+      }
+    } finally {
       setIsLoading(false);
     }
   };
@@ -108,73 +149,99 @@ const SignInForm = () => {
   const progress = Math.floor((loadedAssetCount * 100) / assetLength);
 
   return (
-    <form
-      className="flex flex-col gap-8 py-4 rounded-xl"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col">
-        <label>{m("email")}</label>
-        <input
-          type="email"
-          className={`border border-gray-400 rounded p-2 mt-3 ${
-            errors.email && "border-red-400"
-          }`}
-          {...register("email", {
-            required: m("writeEmail"),
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: m("checkEmail"),
-            },
-          })}
-        />
-        {errors.email && (
-          <span className="text-sm text-red-400 mt-2">
-            {errors.email.message}
-          </span>
-        )}
-      </div>
-
-      <div className="flex flex-col">
-        <label>{m("password")}</label>
-        <input
-          type="password"
-          className={`border border-gray-400 rounded p-2 mt-3 ${
-            errors.password && "border-red-400"
-          }`}
-          {...register("password", {
-            required: m("writePassword"),
-          })}
-        />
-        {errors.password && (
-          <span className="text-sm text-red-400 mt-2">
-            {errors.password.message}
-          </span>
-        )}
-        <div className="flex justify-end mt-1">
-          <Link href="/forgot-password" className="underline text-gray-600">
-            {m("forgotPassword")}
-          </Link>
+    <>
+      {isDuplicated && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center z-50">
+          <div className="bg-white p-4 shadow-lg rounded-xl flex flex-col items-center justify-center gap-4">
+            <p>Duplicated Login</p>
+            <div className="flex gap-2">
+              <button
+                className="bg-blue-400 px-2 py-1 rounded font-bold text-white disabled:bg-gray-200"
+                onClick={() => signIn(watch())}
+                disabled={isPending}
+              >
+                login
+              </button>
+              <button
+                className="bg-red-400 px-2 py-1 rounded font-bold text-white disabled:bg-gray-200"
+                onClick={() => setIsDuplicated(false)}
+                disabled={isPending}
+              >
+                close
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-
-      {isSignInFailed && (
-        <p className="text-sm text-red-400 text-center">{m("failSignIn")}</p>
       )}
 
-      <button
-        type="submit"
-        className="w-full bg-sky-500 rounded-lg py-3 px-2 text-white font-bold disabled:bg-gray-400"
-        disabled={isLoading}
+      <form
+        className="flex flex-col gap-8 py-4 rounded-xl"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        {isImageLoading &&
-          (assetLength === 0 ? (
-            m("getAsset")
-          ) : (
-            <ProgressBar progress={progress} />
-          ))}
-        {!isImageLoading && m("signIn")}
-      </button>
-    </form>
+        <div className="flex flex-col">
+          <label>{m("email")}</label>
+          <input
+            type="email"
+            className={`border border-gray-400 rounded p-2 mt-3 ${
+              errors.email && "border-red-400"
+            }`}
+            {...register("email", {
+              required: m("writeEmail"),
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: m("checkEmail"),
+              },
+            })}
+          />
+          {errors.email && (
+            <span className="text-sm text-red-400 mt-2">
+              {errors.email.message}
+            </span>
+          )}
+        </div>
+
+        <div className="flex flex-col">
+          <label>{m("password")}</label>
+          <input
+            type="password"
+            className={`border border-gray-400 rounded p-2 mt-3 ${
+              errors.password && "border-red-400"
+            }`}
+            {...register("password", {
+              required: m("writePassword"),
+            })}
+          />
+          {errors.password && (
+            <span className="text-sm text-red-400 mt-2">
+              {errors.password.message}
+            </span>
+          )}
+          <div className="flex justify-end mt-1">
+            <Link href="/forgot-password" className="underline text-gray-600">
+              {m("forgotPassword")}
+            </Link>
+          </div>
+        </div>
+
+        {isSignInFailed && (
+          <p className="text-sm text-red-400 text-center">{m("failSignIn")}</p>
+        )}
+
+        <button
+          type="submit"
+          className="w-full bg-sky-500 rounded-lg py-3 px-2 text-white font-bold disabled:bg-gray-400"
+          disabled={isLoading}
+        >
+          {isImageLoading &&
+            (assetLength === 0 ? (
+              m("getAsset")
+            ) : (
+              <ProgressBar progress={progress} />
+            ))}
+          {!isImageLoading && m("signIn")}
+        </button>
+      </form>
+    </>
   );
 };
 
