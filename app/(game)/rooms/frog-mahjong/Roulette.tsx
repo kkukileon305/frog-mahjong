4d0445db7af87019de0cc745e335cca0009379e5
kkukileon305 <kkukileon305@gmail.com>
2024-12-06 19:11:28 +0900

    create roulette effect

---
+++ app/(game)/rooms/frog-mahjong/Roulette.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import RouletteLoading from "@/public/effects/roulette_loading.gif";
+import Roulette0 from "@/public/effects/roulette0.png";
+import Roulette1 from "@/public/effects/roulette1.png";
+import Roulette2 from "@/public/effects/roulette2.png";
+import Roulette3 from "@/public/effects/roulette3.png";
+import delay from "@/utils/functions/delay";
+import { STARTRequest } from "@/utils/constants/frog-mahjong/socketTypes";
+import useFrogMahjongStore from "@/utils/stores/frog-mahjong/useFrogMahjongStore";
+import { getCookie } from "cookies-next";
+
+const Roulette = () => {
+  const gameStore = useFrogMahjongStore();
+  const currentMissions = gameStore.allMissions.filter((m) =>
+    gameStore.gameState?.gameInfo?.missionIDs?.includes(m.id)
+  );
+
+  const userID = getCookie("userID") as string;
+  const currentUser = gameStore.gameState?.users?.find(
+    (user) => user.id === Number(userID)
+  );
+
+  // 룰렛효과
+  const [step, setStep] = useState(0);
+  const slidingContainer = useRef<HTMLDivElement>(null);
+
+  const rouletteSteps = [Roulette0, Roulette1, Roulette2, Roulette3];
+
+  const allTitles = gameStore.allMissions;
+  const lieTitles = [...allTitles, ...allTitles];
+
+  const titles = currentMissions;
+
+  const indexes = currentMissions.map((_, idx) =>
+    gameStore.allMissions.findIndex((m) => m.id === currentMissions[idx].id)
+  );
+
+  const titleIndexes = [
+    allTitles.length + indexes[0],
+    +indexes[1],
+    allTitles.length + indexes[2],
+  ];
+
+  useEffect(() => {
+    const spinRoulette = async () => {
+      if (gameStore.allMissions.length === 0) return;
+
+      slidingContainer.current &&
+        (slidingContainer.current.style.transform = `translateY(-${titleIndexes[0]}00%)`);
+      await delay(1000);
+      setStep(1);
+
+      await delay(1000);
+
+      slidingContainer.current &&
+        (slidingContainer.current.style.transform = `translateY(-${titleIndexes[1]}00%)`);
+      await delay(1000);
+      setStep(2);
+
+      await delay(1000);
+
+      slidingContainer.current &&
+        (slidingContainer.current.style.transform = `translateY(-${titleIndexes[2]}00%)`);
+      await delay(1000);
+      setStep(3);
+
+      await delay(5000);
+
+      if (!currentUser?.isOwner) return;
+
+      const request: STARTRequest = {
+        roomID: Number(gameStore.gameState?.gameInfo?.roomID),
+        event: "START",
+        message: "",
+      };
+
+      gameStore.ws?.send(JSON.stringify(request));
+    };
+
+    spinRoulette();
+  }, [gameStore.allMissions]);
+
+  return (
+    <div className="fixed top-0 left-0 w-full bg-[#FCE4C0] h-lvh justify-center items-center flex flex-col p-4 z-30">
+      <div className="max-w-[320px] overflow-hidden">
+        <div className="relative">
+          <p className="absolute top-[5%] left-1/2 -translate-x-1/2 font-bold text-2xl">
+            랜덤미션
+          </p>
+          <img src={rouletteSteps[step].src} alt="rout1" />
+
+          <div className="absolute left-1/2 -translate-x-1/2 w-[80%] top-[28%] h-[22%] overflow-hidden">
+            <div
+              ref={slidingContainer}
+              className="h-full transition-transform duration-1000"
+              style={{ transform: "translateY(0%)" }}
+            >
+              {lieTitles.map((title, index) => (
+                <div
+                  key={index}
+                  className="w-full text-center h-full flex items-center justify-center"
+                >
+                  {title.title}
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {titles.map((title, idx) => (
+            <p
+              key={title.id}
+              className="w-[80%] text-nowrap overflow-hidden text-center absolute left-1/2 -translate-x-1/2"
+              style={{
+                bottom: `calc(${2 - idx}8%)`,
+                display: idx < step ? "block" : "none",
+              }}
+            >
+              {title.title}
+            </p>
+          ))}
+        </div>
+      </div>
+      <img src={RouletteLoading.src} alt="roulette loading" />
+    </div>
+  );
+};
+
+export default Roulette;
