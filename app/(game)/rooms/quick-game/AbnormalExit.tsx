e404188ff793b1afc0f4cbd38501493bafbc1e6a
kkukileon305 <kkukileon305@gmail.com>
2024-12-24 18:41:25 +0900

    redirect rooms in abnormal Modal after 3000ms

--- app/(game)/rooms/quick-game/AbnormalExit.tsx
+++ app/(game)/rooms/quick-game/AbnormalExit.tsx
@@ -19,13 +19,13 @@ const AbnormalExit = () => {
   const router = useRouter();
 
   useEffect(() => {
-    // const timeout = setTimeout(() => {
-    //   router.push("/rooms");
-    //
-    //   if (process.env.NODE_ENV === "development") {
-    //     console.log("abnormal redirect");
-    //   }
-    // }, 3000);
+    const timeout = setTimeout(() => {
+      router.push("/rooms");
+
+      if (process.env.NODE_ENV === "development") {
+        console.log("abnormal redirect");
+      }
+    }, 3000);
 
     localStorage.removeItem("matchMode");
     localStorage.removeItem("sessionID");
@@ -34,7 +34,7 @@ const AbnormalExit = () => {
     localStorage.removeItem("victoryFailed");
 
     return () => {
-      // clearTimeout(timeout);
+      clearTimeout(timeout);
 
       oldClear();
       clear();
@@ -48,7 +48,7 @@ const AbnormalExit = () => {
       <div className="p-4 w-full max-w-3xl rounded-xl flex justify-center items-center flex-col">
         <p className="font-bold text-3xl">{m("title")}</p>
         <Link
-          className="bg-white py-2 px-4 mt-4 border border-blue-400 rounded-xl text-blue-400 font-bold"
+          className="flex bg-white py-2 text-center px-4 mt-4 border border-blue-400 rounded-xl text-blue-400 font-bold"
           href={"/rooms"}
         >
           {m("back")}
